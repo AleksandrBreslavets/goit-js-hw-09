f71d29e053ca5769bfec7c730a81cdb3fe1e63e9
AleksandrBreslavets <sasha.bresl2612005@gmail.com>
2023-04-16 01:46:46 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+    body: document.querySelector("body"),
+}
+
+refs.btnStart.addEventListener("click", onStartClick);
+refs.btnStop.addEventListener("click", onStopClick);
+
+let timerId = null;
+refs.btnStop.setAttribute("disabled", "");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+function onStartClick() {
+    timerId=setInterval(() => {
+        const color = getRandomHexColor();
+        refs.body.style.backgroundColor = `${color}`;
+    }, 1000);
+    refs.btnStart.setAttribute("disabled", "");
+    refs.btnStop.removeAttribute("disabled");
+    
+}
+function onStopClick() {
+    clearInterval(timerId);
+        refs.btnStop.setAttribute("disabled", "");
+        refs.btnStart.removeAttribute("disabled");
+}
+
 
